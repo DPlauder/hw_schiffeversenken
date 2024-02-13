58554e7ad0f5e5342a317369673e1d792be72e79
Dominik <docgit@gmail.comgit commit -m first>
2024-02-13 13:53:21 +0100

    update

--- src/index.js
+++ src/index.js
@@ -15,12 +15,13 @@ const player = new Player("Marko", gameboardKI);
 // Testschiffe
 const ships = [];
 const carrier = new Ship("carrier", 5, 5, "h");
+/* 
 const battleship = new Ship("battleship", 4, 4, "h");
 const cruiser = new Ship("cruiser", 3, 3, "h");
 const submarine = new Ship("submarine", 3, 2, "h");
 const destroyer = new Ship("Destroyer", 2, 1, "h");
-
-ships.push(carrier, battleship, cruiser, submarine, destroyer);
+ */
+ships.push(carrier /* , battleship, cruiser, submarine, destroyer */);
 
 gameboardPlayer.createShipsCPU(ships);
 gameboardPlayer.placeShipsCPU();
@@ -31,6 +32,7 @@ console.log("helloKi", gameboardKI);
 
 const newGameBoardView = new GameboardView();
 newGameBoardView.showShips(gameboardKI.getGameBoard());
+newGameBoardView.updateViewBoard(gameboardKI.getGameBoard());
 
 //zum testen geadded
 let shot = 0;
